d500a173023c70c20693819ccc7e34ace95444f7
Ioan Moldovan <ioanm@linux.com>
2022-07-26 13:14:34 +0300

    Initial Commit

---
+++ UPSnut Pro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UPSnut Pro
+//
+//  Created by Ioan Moldovan on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
